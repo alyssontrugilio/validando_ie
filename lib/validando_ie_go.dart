da641d8b9fbd50a8cdcedc63d5fc028583c5acda
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-06-24 09:50:11 -0300

    refacto: ajustado validação do estado de GO implementando tudo na mesma fução

--- lib/validando_ie_go.dart
+++ lib/validando_ie_go.dart
@@ -7,33 +7,32 @@ bool checkInscricaoEstadualGO(String inscricaoEstadual) {
   if (!(inicio == 10 || inicio == 11 || (inicio >= 20 && inicio <= 29))) {
     return false;
   }
+  bool calculaDigito(String inscricaoEstadual) {
+    int soma = 0;
+    int peso = 9;
+
+    for (int i = 0; i < 8; i++) {
+      soma += int.parse(inscricaoEstadual[i]) * peso;
+      peso--;
+    }
+
+    int resto = soma % 11;
+    int digitoVerificador;
+
+    if (resto == 0 || resto == 1) {
+      digitoVerificador = 0;
+    } else {
+      digitoVerificador = 11 - resto;
+    }
+
+    return digitoVerificador == int.parse(inscricaoEstadual[8]);
+  }
 
   return calculaDigito(inscricaoEstadual);
 }
 
-bool calculaDigito(String inscricaoEstadual) {
-  int soma = 0;
-  int peso = 9;
-
-  for (int i = 0; i < 8; i++) {
-    soma += int.parse(inscricaoEstadual[i]) * peso;
-    peso--;
-  }
-
-  int resto = soma % 11;
-  int digitoVerificador;
-
-  if (resto == 0 || resto == 1) {
-    digitoVerificador = 0;
-  } else {
-    digitoVerificador = 11 - resto;
-  }
-
-  return digitoVerificador == int.parse(inscricaoEstadual[8]);
-}
-
 void main() {
   print(
-    checkInscricaoEstadualGO("124444652"),
+    checkInscricaoEstadualGO("200626302"),
   );
 }
